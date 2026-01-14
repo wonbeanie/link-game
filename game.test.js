c24fb6030622e3dd4d83428b6e983c6b003c6d86
wonbeanie <goobb2580@gmail.com>
2026-01-14 17:18:08 +0900

    방장 기능 및 닉네임 설정 케이스 추가
    1. 방장 기능인 게임 시작과 데이터베이스 초기화 버튼 테스트 케이스 추가
    2. 다른 유저가 존재할때의 테스트 케이스 추가

--- game.test.js
+++ game.test.js
@@ -7,34 +7,55 @@ import '@testing-library/jest-dom';
 
 import fs from 'fs';
 import path from 'path';
+import { anotherUserUpdateDatabase } from './__mocks__/mock-firebase-database.js';
 
 jest.mock('https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js');
 jest.mock("https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js");
 
 describe('테스트', () => {
   const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), 'utf8');
-  document.body.innerHTML = html.toString();
-
-  beforeEach(() => {
+  const userNickname = "유저";
+  const nickname = "방장";
+  const gameDataTable = "GameData/";
+  let initDatabase = {};
+  
+  beforeEach(()=> {
+    window.history.pushState({}, '', '?admin=true');
+    document.body.innerHTML = html.toString();
+    jest.resetModules();
     import("./game.js");
+
+    initDatabase = {};
+    initDatabase[gameDataTable] = {};
+    anotherUserUpdateDatabase(initDatabase);
   });
 
-  test('닉네임 변경 확인', () => {
-    const input = screen.getByPlaceholderText('닉네임을 입력하세요');
+  test("게임 시작과 초기화 버튼 확인", () => {
+    const gameStartBtn = screen.getByText("게임 시작하기");
+    const databaseInitBtn = screen.getByText("초기화");
 
-    expect(input).toBeInTheDocument();
+    expect(gameStartBtn).toBeVisible();
+    expect(databaseInitBtn).toBeVisible();
   });
 
-  test("닉네임 설정 확인", async () => {
+  test("1명의 유저가 존재할때 닉네임 설정 확인", async () => {
+    initDatabase[gameDataTable][userNickname] = "Ready";
+    anotherUserUpdateDatabase(initDatabase);
+
     const nicknameInput = screen.getByPlaceholderText('닉네임을 입력하세요');
-    const nickname = "테스트";
     fireEvent.change(nicknameInput, {target : {value : nickname}});
 
     const confirmButton = screen.getByText("입력 완료");
     confirmButton.click();
 
-    let items = await screen.findAllByText(nickname);
-
+    const items = await screen.findAllByText(nickname);
     expect(items).toHaveLength(2);
-  })
+
+    const userItems = await screen.findAllByText(userNickname);
+    expect(userItems).toHaveLength(1);
+
+    const readyItems = await screen.findAllByText("Ready");
+    expect(readyItems).toHaveLength(2);
+  });
+  
 });
