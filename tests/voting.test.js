2844817510b86a1d3d1601ffeb7f785dbcdc1e89
wonbeanie <goobb2580@gmail.com>
2026-01-17 18:23:03 +0900

    플레이어 투표 테스트 케이스 추가
    1. 정상적인 플레이어 투표에 대한 테스트 케이스를 추가하였습니다.

--- tests/voting.test.js
+++ tests/voting.test.js
@@ -1,14 +1,50 @@
-import { screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, within } from "@testing-library/dom";
 import { setupGameStart, setupHTMLInit, setupSendHint } from "./modules/game-helpers";
+import { nickname, userNickname } from "./__mocks__/mock-firebase-database";
 
-describe("토론 및 투표 테스트", () => {
+describe("투표 테스트", () => {
   beforeEach(async ()=>{
     await setupHTMLInit();
     await setupGameStart();
     await setupSendHint();
   });
 
-  test("토론 및 투표 팝업 확인", async () => {
-    screen.findByText(/범인 지목 투표/);
+  test("정상적인 플레이어 투표", async () => {
+    screen.getByText(/범인 지목 투표/);
+
+    const suspectSelectLabel = screen.getByText(/범인 지목 투표/);
+
+    const votingSelect = suspectSelectLabel.nextElementSibling;
+
+    expect(votingSelect).toHaveAttribute("id", "player-select");
+    expect(votingSelect.selectedOptions[0].textContent).toBe(nickname);
+    expect(votingSelect.selectedOptions[0].selected).toBe(true);
+    expect(votingSelect.selectedOptions[0]).toBeVisible();
+
+    fireEvent.change(votingSelect, {
+      target : { value: userNickname }
+    });
+
+    const votingSelectOption = screen.getByRole('option', {
+      name : userNickname
+    });
+
+    expect(votingSelectOption.selected).toBe(true);
+
+    const votingBtn = screen.getByText("투표 완료");
+
+    votingBtn.click();
+
+    const activityLog = screen.getByText(/활동 로그/);
+    const logDisplay = activityLog.nextElementSibling;
+
+    expect(logDisplay).toHaveAttribute('id', 'display');
+
+    const { findByText, findAllByText } = within(logDisplay);
+    const nicknameLog = await findAllByText(nickname);
+    expect(nicknameLog).toHaveLength(2);
+
+    const votingLog = await findByText(`${userNickname}님을 투표하였습니다.`);
+    expect(votingLog).toBeVisible();
   });
 })
