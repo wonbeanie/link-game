19f18562d68d974d397fa42fcc1b6f5adc8a274e
wonbeanie <goobb2580@gmail.com>
2026-01-22 03:00:04 +0900

    플레이어 수에 따라 투표 테스트 구조 분리
    - 투표가 동점일때 다양한 케이스를 위해 분리하였습니다.
    - 투표가 동점 케이스는 추후에 플레이어 설정이 가능할때 추가

--- tests/voting.test.js
+++ tests/voting.test.js
@@ -1,95 +1,108 @@
 import { fireEvent, screen, within } from "@testing-library/dom";
 import { checkAlert, setupGameStart, setupHTMLInit, setupSendHint } from "./modules/game-helpers";
-import { nickname, userNickname } from "./__mocks__/mock-firebase-database";
+import { nickname, secondNickname, thirdNickname, userNickname } from "./__mocks__/mock-firebase-database";
 import { setupVoting, setupVotingSetting } from "./modules/vote-helpers";
 
 describe("투표 테스트", () => {
   beforeEach(async ()=>{
     jest.useFakeTimers();
     await setupHTMLInit();
-    await setupGameStart();
-    await setupSendHint();
   });
 
   afterEach(() => {
     jest.useRealTimers();
   })
 
-  test("정상적인 플레이어 투표", async () => {
-    screen.getByText(/범인 지목 투표/);
-
-    const suspectSelectLabel = screen.getByText(/범인 지목 투표/);
-
-    const votingSelect = suspectSelectLabel.nextElementSibling;
-
-    expect(votingSelect).toHaveAttribute("id", "player-select");
-    expect(votingSelect.selectedOptions[0].textContent).toBe(nickname);
-    expect(votingSelect.selectedOptions[0].selected).toBe(true);
-    expect(votingSelect.selectedOptions[0]).toBeVisible();
-
-    fireEvent.change(votingSelect, {
-      target : { value: userNickname }
+  describe("플레이어 2명인 경우", () => {
+    beforeEach(async ()=>{
+      await setupGameStart();
+      await setupSendHint();
     });
 
-    const votingSelectOption = screen.getByRole('option', {
-      name : userNickname
-    });
+    test("정상적인 플레이어 투표", async () => {
+      screen.getByText(/범인 지목 투표/);
 
-    expect(votingSelectOption.selected).toBe(true);
+      const suspectSelectLabel = screen.getByText(/범인 지목 투표/);
 
-    const votingBtn = screen.getByText("투표 완료");
+      const votingSelect = suspectSelectLabel.nextElementSibling;
 
-    votingBtn.click();
+      expect(votingSelect).toHaveAttribute("id", "player-select");
+      expect(votingSelect.selectedOptions[0].textContent).toBe(nickname);
+      expect(votingSelect.selectedOptions[0].selected).toBe(true);
+      expect(votingSelect.selectedOptions[0]).toBeVisible();
 
-    const activityLog = screen.getByText(/활동 로그/);
-    const logDisplay = activityLog.nextElementSibling;
-
-    expect(logDisplay).toHaveAttribute('id', 'display');
-
-    const { findByText, findAllByText } = within(logDisplay);
-    const nicknameLog = await findAllByText(nickname);
-    expect(nicknameLog).toHaveLength(2);
-
-    const votingLog = await findByText(`${userNickname}님을 투표하였습니다.`);
-    expect(votingLog).toBeVisible();
-  });
-
-  describe("투표 종료 테스트", () => {
-    describe("범인인것을 들킨 경우", () => {
-      test.each([
-        { 
-          desc: "시민의 키워드을 틀렸을때", 
-          suspectAnswerSetting: "가짜 정답",
-          expectedAlertText: "시민 승리"
-        },
-        { 
-          desc: "시민의 키워드을 맞췄을때", 
-          suspectAnswerSetting: "정답",
-          expectedAlertText: "범인 승리"
-        },
-      ])("$desc", async ({ suspectAnswerSetting, expectedAlertText }) => {
-        await setupVoting(setupVotingSetting.SUSPECT_EXPOSED);
-        const suspectAnswer = screen.getByPlaceholderText("시민의 정답은?");
-        expect(suspectAnswer).toBeVisible();
-        fireEvent.change(suspectAnswer, {
-          target : { value: suspectAnswerSetting }
-        });
-        const suspectAnswerBtn = screen.getByText("정답 맞추기");
-        suspectAnswerBtn.click();
-        await checkAlert(expectedAlertText);
+      fireEvent.change(votingSelect, {
+        target : { value: userNickname }
       });
+
+      const votingSelectOption = screen.getByRole('option', {
+        name : userNickname
+      });
+
+      expect(votingSelectOption.selected).toBe(true);
+
+      const votingBtn = screen.getByText("투표 완료");
+
+      votingBtn.click();
+
+      const activityLog = screen.getByText(/활동 로그/);
+      const logDisplay = activityLog.nextElementSibling;
+
+      expect(logDisplay).toHaveAttribute('id', 'display');
+
+      const { findByText, findAllByText } = within(logDisplay);
+      const nicknameLog = await findAllByText(nickname);
+      expect(nicknameLog).toHaveLength(2);
+
+      const votingLog = await findByText(`${userNickname}님을 투표하였습니다.`);
+      expect(votingLog).toBeVisible();
     });
 
-    test("투표가 동점일 경우", async () => {
-      await setupVoting(setupVotingSetting.TIE_VOTES);
-    })
+    describe("투표 종료 테스트", () => {
+      describe("범인인것을 들킨 경우", () => {
+        test.each([
+          { 
+            desc: "시민의 키워드을 틀렸을때", 
+            suspectAnswerSetting: "가짜 정답",
+            expectedAlertText: "시민 승리"
+          },
+          { 
+            desc: "시민의 키워드을 맞췄을때", 
+            suspectAnswerSetting: "정답",
+            expectedAlertText: "범인 승리"
+          },
+        ])("$desc", async ({ suspectAnswerSetting, expectedAlertText }) => {
+          await setupVoting(setupVotingSetting.SUSPECT_EXPOSED);
+          const suspectAnswer = screen.getByPlaceholderText("시민의 정답은?");
+          expect(suspectAnswer).toBeVisible();
+          fireEvent.change(suspectAnswer, {
+            target : { value: suspectAnswerSetting }
+          });
+          const suspectAnswerBtn = screen.getByText("정답 맞추기");
+          suspectAnswerBtn.click();
+          await checkAlert(expectedAlertText);
+        });
+      });
 
-    test("범인을 찾은 경우", async () => {
-      await setupVoting(setupVotingSetting.FOUND_SUSPECT);
-    })
+      test("범인을 찾은 경우", async () => {
+        await setupVoting(setupVotingSetting.FOUND_SUSPECT);
+      })
 
-    test("범인을 찾지 못한 경우", async () => {
-      await setupVoting(setupVotingSetting.NOTFOUND_SUSPECT);
+      test("범인을 찾지 못한 경우", async () => {
+        await setupVoting(setupVotingSetting.NOTFOUND_SUSPECT);
+      })
     })
   })
+
+  // todo
+  // describe("플레이어가 4인인 경우", () => {
+  //   beforeEach(async ()=>{
+  //     await setupGameStart();
+  //     await setupSendHint();
+  //   });
+
+  //   test("투표가 동점일 경우", async () => {
+  //     await setupVoting(setupVotingSetting.TIE_VOTES);
+  //   })
+  // })
 })
